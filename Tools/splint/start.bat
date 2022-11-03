ebb974f71957fdabcb48445b166f147991128b68
Philipp Pawlischinez <philipp.pawlischinez@fh-swf.de>
2022-11-03 08:58:02 +0100

    Modelbased Variant

--- Tools/splint/start.bat
+++ Tools/splint/start.bat
@@ -2,9 +2,10 @@
 
 set defines=-DSPLINT -Dinterrupt=""
 
-splint.exe Main.c %defines% >> log.txt 2>&1
+splint.exe ert_main.c %defines% >> log.txt 2>&1
 splint.exe Taster.c %defines% >> log.txt 2>&1
 splint.exe Timer.c %defines% >> log.txt 2>&1
 splint.exe Init.c %defines% >> log.txt 2>&1
+splint.exe Blinkersteuerung.c %defines% >> log.txt 2>&1
 exit 0
 pause
